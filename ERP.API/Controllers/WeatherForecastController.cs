dd9756367f96dfbc88c7d03c0e1ff4af3daacdfd
Edionaldo Costa <edionaldo.costa@mobicare.com.br>
2019-12-11 23:15:47 -0300

    Add Model

--- ERP.API/Controllers/WeatherForecastController.cs
+++ ERP.API/Controllers/WeatherForecastController.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ERP.API.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace ERP.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -23,17 +19,52 @@ namespace ERP.API.Controllers
             _logger = logger;
         }
 
+        // localhost:5000/api/weatherforecast
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult<IEnumerable<Evento>> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return new Evento[] {
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular 1",
+                    Local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular 3",
+                    Local = "Rio de Janeiro",
+                    Lote = "1º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                },
+            };
+        }
+        
+        // localhost:5000/api/weatherforecast/2
+        [HttpGet("{id}")]
+        public ActionResult<Evento> GetAction(int id)
+        {
+            return new Evento[] {
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular 1",
+                    Local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular 3",
+                    Local = "Rio de Janeiro",
+                    Lote = "1º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                },
+            }.FirstOrDefault(x => x.EventoId == id);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ERP
+
+# GIT
+
+## …or create a new repository on the command line
+echo "# ERP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/edionaldo/ERP.git
+git push -u origin master
+
+## …or push an existing repository from the command line
+git remote add origin https://github.com/edionaldo/ERP.git
+git push -u origin master
